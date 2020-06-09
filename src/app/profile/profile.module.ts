77d312fbe4682737b33607aed6ef843a9821d933
gupta123456 <anujg0046@gmail.com>
2020-06-09 22:32:03 +0530

    portfolio project

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -8,7 +8,6 @@ import { FooterComponent } from './footer/footer.component';
 import { IntroComponent } from './intro/intro.component';
 import { AboutComponent } from './about/about.component';
 import { SkillsComponent } from './skills/skills.component';
-import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ExperienceComponent } from './experience/experience.component';
 import { EducationComponent } from './education/education.component';
 import { ReferenceComponent } from './reference/reference.component';
@@ -30,7 +29,6 @@ import { ProjectsComponent } from './projects/projects.component';
     IntroComponent,
     AboutComponent,
     SkillsComponent,
-    PortfolioComponent,
     ExperienceComponent,
     EducationComponent,
     ReferenceComponent,
